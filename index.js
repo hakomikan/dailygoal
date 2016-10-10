b947da99c97175c269283921dca790eb1980381e
hakomikan <mail@s-taki.sakura.ne.jp>
2016-10-10 03:42:04 +0000

    pretiffy index page

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var pug = require('pug');
+var path = require("path"); 
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 app.listen(app.get('port'), function() {
